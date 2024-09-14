34c9dffea7bda57d3971717773ea207fb1bc4bd4
joo93 <joo93d.o@gmail.com>
2024-09-14 20:44:36 +0900

    API 프록시 설정 및 URL 수정

--- src/store/products.ts
+++ src/store/products.ts
@@ -5,6 +5,8 @@ import CONSTANTS from "../constants/constants";
 // const productsURL = '/products.json';
 const productsURL = `${CONSTANTS.IS_DEV ? `/proxy` : `${import.meta.env.VITE_FAKE_STORE_API}`}/products`;
 
+console.log("API request URL:", productsURL);
+
 interface IRating {
   readonly rate?: number;
   readonly count?: number;
